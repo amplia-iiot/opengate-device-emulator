fa7e9b494359225d5f1850332b88c7d93af26dfa
Scarfpurple <scarfpurple97@gmail.com>
2020-10-21 11:40:39 +0200

    codemirror v0.1

--- components/configuracion.vue
+++ components/configuracion.vue
@@ -1,54 +1,79 @@
 <template>
   <div>
     <v-app>
-      <h3>Configuracion</h3>
-      <v-expansion-panels>
-        <v-expansion-panel>
-          <v-expansion-panel-header> Formatear </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-switch
-              v-model="config"
-              :label="`¿Configuracion?: ${config.toString()}`"
-            ></v-switch>
-            <v-switch
-              v-model="datos"
-              :label="`¿Datos?: ${datos.toString()}`"
-            ></v-switch>
-            <v-switch
-              v-model="todo"
-              :label="`¿Todo?: ${todo.toString()}`"
-            ></v-switch>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-      <v-switch v-model="update" :label="`¿Actualizar Software?`"></v-switch>
-      <v-row>
-        <v-col cols="4" md="1">
-          <v-text-field v-model="nombre" label="Nombre"></v-text-field>
-        </v-col>
-        <v-col cols="3" md="1">
-          <v-text-field v-model="fecha" label="Fecha"></v-text-field>
-        </v-col>
-        <v-col cols="2" md="1">
-          <v-text-field v-model="hora" label="Hora"></v-text-field>
-        </v-col>
-      </v-row>
+      <v-autocomplete
+          style="color: red"
+          :items="items"
+          label="Oeration to make:"
+          required
+          v-model="model"
+        ></v-autocomplete>
+        <codemirror
+         :options="cmOptions"
+          v-model="code"
+          
+        />
+        <v-btn @click="send" class="send">
+          Send
+        </v-btn>
+        <v-btn @click="reset" class="reset">
+          Reset
+        </v-btn>
+      
     </v-app>
   </div>
 </template>
 <script>
+import "codemirror/mode/javascript/javascript.js";
+import { codemirror } from 'vue-codemirror-lite';
+import "codemirror/theme/base16-dark.css";
 export default {
+  components: {
+    codemirror,
+  },
+
   name: "configuracion",
   data() {
     return {
-      config: true,
-      datos: false,
-      todo: false,
-      update: false,
-      nombre : "Chema",
-      fecha: "1/10/2020",
-      hora: "08:00 AM",
-    };
+      code: '',
+     cmOptions: {
+        tabSize: 4,
+        styleActiveLine: true,
+        lineNumbers: true,
+        line: true,
+        mode: 'text/javascript',
+        lineWrapping: true,
+        theme: 'default'
+          
+        },
+    model : null,
+    items:[
+      "Reset equipment",
+      "Reboot equipment",
+      "Turn dowm device",
+      "Turn on device",
+      "Reset sensors",
+    
+    ]
+    }
   },
+  methods:{
+    send(){
+      alert("Operation send with your javascript thank you!");
+    },
+    reset(){
+      this.code="";
+    }
+  }
+
+ 
 };
 </script>
+<style scoped>
+.send{
+  float:left;
+}
+.reset{
+  float:right;
+}
+</style>
